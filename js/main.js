dd3146346015cd9954a2a8c72a4819d384805190
RYAN JOHN DEAN <rjd9se@virginia.edu>
2016-04-13 19:26:19 -0400

    cleaned house

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// include dependencies
-<script src="./three.min.js"></script>									// main Three.js module (minified)
-<script src="./js/StereoEffect.js"></script>							// allows for stereoscopic effect
-<script src="./js/DeviceOrientationControls.js"></script>				// allows us to use the device's accelerometer
-<script src="./js/OrbitControls.js"></script>							// allows us to test with a mouse
-<script src="./js/helvetiker_regular.typeface.js"></script>				// a font
